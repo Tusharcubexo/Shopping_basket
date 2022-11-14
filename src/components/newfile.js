799b94b935e28fbb2b9f3d7673e0bb866f645a94
vandana9cubexo <vandana.rathod@cubexo.io>
2022-11-14 18:01:53 +0530

    cherry pick commit

--- src/components/newfile.js
+++ src/components/newfile.js
@@ -1,9 +1,18 @@
+<<<<<<< HEAD
 export const NewFileForTesting = () => {
     return (
         <>
             <h1>
                 my name
             </h1>
+=======
+const NewFileForTesting = ()=>{
+    return(
+        <>
+        <h1>
+            my name 
+        </h1>
+>>>>>>> 685e9a2 (first squesh commit)
         </>
     )
 }
